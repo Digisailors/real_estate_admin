19a62517012ed594bb826423cbfd4410b99b5aca
Vetrivelu <vetrivelu25@gmail.com>
2023-04-23 09:57:19 +0530

    removed "all" in enum

--- lib/Model/Agent.dart
+++ lib/Model/Agent.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:real_estate_admin/Model/Staff.dart';
 import 'package:real_estate_admin/Providers/session.dart';
 
-enum ActiveStatus {pendingApproval, active, blocked, all }
+enum ActiveStatus { pendingApproval, active, blocked }
 
 class Agent {
   int leadCount;
